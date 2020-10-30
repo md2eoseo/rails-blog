4a628f5971703039eec485509ecf7e5781858d3b
md2eoseo <md2eoseo@gmail.com>
2020-10-30 13:26:53 +0900

    comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    
     def index
         @articles = Article.all
     end
@@ -41,9 +44,9 @@ class ArticlesController < ApplicationController
         
         redirect_to articles_path
     end
+    
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
-
-private
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true,
-                    length: { minimum: 1 }
+    has_many :comments, dependent: :destroy
+    validates :title, presence: true, length: { minimum: 1 }
 end
